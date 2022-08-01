eddb519a7cdda3c7ba4ebf5040f591ed2632d4e1
Thiago Lira <thlira15@gmail.com>
2022-08-01 09:59:57 -0300

    remove unnecessary java path

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :dependencies [[org.clojure/clojure "1.10.3"]
                [overtone "0.10.6"]]
   :native-path "native"
-  :java-cmd "/Library/Java/JavaVirtualMachines/jdk-18.0.2.jdk/Contents/Home/bin/java"
   :source-paths ["src"])
